c7824e55fd88d0b9c52d8e8e351a674cd86e235f
Dylan Chen <hjchen2005@gmail.com>
2015-11-20 15:57:03 -0500

    Added error messages

--- TryCatch.cs
+++ TryCatch.cs
@@ -3,7 +3,7 @@ execute all the statements inside the try block, and if none of the statements g
 exception, they all run, one after the other, to completion */
 
 using System;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 
 namespace TryCatch{
     public class Main (){
@@ -14,10 +14,13 @@ namespace TryCatch{
             int answer=lhs-rhs;
             result.Text = answer.ToString();
         }catch (FormatException fEx){
-            DialogueResult res = MessageBox.Show("Format Exception");
+           /* DialogueResult res = MessageBox.Show("Format Exception");
             if (res == System.Windows.Forms.DialogueResult.Yes){
                 this.close();
-            }
+            }*/
+            //Message property contains a text description of the error that caused the exception.
+            Console.WriteLine("Error: ",fEx.Message); 
+            Console.WriteLine("If you need any assistance, visit ",fEx.HelpLink);
         }
   }
 }
