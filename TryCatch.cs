d8dc0628fa753cedcf19255efb22f0a89161e40d
Dylan Chen <hjchen2005@gmail.com>
2015-11-20 15:18:24 -0500

    Create TryCatch.cs

---
+++ TryCatch.cs
@@ -0,0 +1,23 @@
+/*Write your code inside a try block (try is a C# keyword). When the code runs, it attempts to
+execute all the statements inside the try block, and if none of the statements generates an
+exception, they all run, one after the other, to completion */
+
+using System;
+using System.Windows.Forms;
+
+namespace TryCatch{
+    public class Main (){
+    String result;
+        try{
+            int lhs=int.Parse(lhsOperand.Text);
+            int rhs=int.Parse(rhsOperand.Text);
+            int answer=lhs-rhs;
+            result.Text = answer.ToString();
+        }catch (FormatException fEx){
+            DialogueResult res = MessageBox.Show("Format Exception");
+            if (res == System.Windows.Forms.DialogueResult.Yes){
+                this.close();
+            }
+        }
+  }
+}
