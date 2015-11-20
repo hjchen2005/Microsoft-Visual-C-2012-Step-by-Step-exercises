1aa6bea3f07d2a41894422aca7eca08dde0a0621
Dylan Chen <hjchen2005@gmail.com>
2015-11-20 17:03:46 -0500

    Create CheckedUnchecked.cs

---
+++ CheckedUnchecked.cs
@@ -0,0 +1,22 @@
+/*A checked statement is a block preceded by the checked keyword. 
+All integer arithmetic in a checked statement always throws an 
+OverflowException if an integer calculation in the block overflows*/
+
+using System;
+
+namespace CheckUncheck{
+  public class CheckUncheck{
+    int num = int.MaxValue;
+    checked{
+        //Only integer arithmetic directly inside the checked block is subject to overflow checking
+        int willThrow =num++; 
+    }
+    unchecked{
+        try{
+            int wontThrow = num++;
+        } catch (OverflowException e){
+            Console.WriteLine("Error: ",e.Message);
+        }
+    }
+  } 
+}
