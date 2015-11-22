cdba116d097b0878cfad2ef0b92cf32fb13ef621
Dylan Chen <hjchen2005@gmail.com>
2015-11-21 23:28:55 -0500

    Update ThrowException.cs

--- ThrowException.cs
+++ ThrowException.cs
@@ -2,19 +2,24 @@ using System;
 using System.ArgumentOutOfRangeException;
 
 namespace ErrMgmt{
-	public class ThrowException{
+	public class ThrowException
+	{
 		public static void Main (string[] args){
 			Console.WriteLine(nameOfMonth(13));
 		}
-		
-		public static string nameOfMonth(int month){
+
+		public static string nameOfMonth (int month)
+		{
+			switch (month) {
 			case 1:
 				return "January";
 			// if month<1 or month>12, method shouldn't return anything
 			// Similar scenario: "Attenders must be 21 or above" etc
 			default:
 			//exceptions caused by invalid arguments that are not null
-				throw new ArgumentOutOfRangeException("Not a month");
+				throw new ArgumentOutOfRangeException ("Not a month");
+			}
 		}
+
 	}
 }
