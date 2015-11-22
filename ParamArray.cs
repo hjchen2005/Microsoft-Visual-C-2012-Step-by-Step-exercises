f87c8b33db23c3139f64cd8e445afea1a075be27
Dylan Chen <hjchen2005@gmail.com>
2015-11-22 00:03:44 -0500

    Update ParamArray.cs

--- ParamArray.cs
+++ ParamArray.cs
@@ -4,6 +4,10 @@ namespace OOP
 {
 	public class ParamArray
 	{
+
+                ParaArray(){
+                        VarArgs(12,13,14,15,16);
+                }
 		public static void VarArgs(int val1, params int[] vals){
 			foreach (int i in vals){
 				Console.WriteLine("vals[]:{0}", vals[]);	
