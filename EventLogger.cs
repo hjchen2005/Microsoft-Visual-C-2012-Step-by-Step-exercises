686b2bf5a010a17ca51d85f0517011c5e4d08300
Dylan Chen <hjchen2005@gmail.com>
2015-11-23 14:54:56 -0500

    Update EventLogger.cs

--- EventLogger.cs
+++ EventLogger.cs
@@ -2,18 +2,22 @@
 using System;
 using System.IO;
 
-public class EventLogger{
-  static private StreamWriter eventLog;
-  static private string strLogName;
-  
-  static EventLogger(){
-    evetLog = File.CreateText("logfile.txt");
-    
-    // statement below will throw an exception
-    strLogName = (string) strLogName.Clone();
-  }
-  
-  static public void WriteLog(string someText){
-    eventLog.Write(someText);
-  }
+public class EventLogger
+    {
+        static private StreamWriter eventLog;
+        static private string strLogName;
+
+        static EventLogger()
+        {
+            eventLog = File.CreateText("logfile.txt");
+
+            // statement below will throw an exception
+            strLogName = (string)strLogName.Clone();
+        }
+
+        static public void WriteLog(string someText)
+        {
+            eventLog.Write(someText);
+        }
+    }
 }
